e164fa5efd2db59b04bdd7dbb6e529f1c30f9980
Tyler Christensen <tcchristensen07@gmail.com>
2018-06-20 22:48:06 -0600

    manipulating classes with js

--- 7. Manipulating CSS Class Properties Using JavaScript/js.js
+++ 7. Manipulating CSS Class Properties Using JavaScript/js.js
@@ -1,22 +1,40 @@
 //creates listeners 
 function startup() {
-    var target = document.getElementsByTagName('figure');
+    /*var target = document.getElementsByClassName('card');
    
     for (let object of target){
         object.addEventListener('mouseover',function(){
-            this.className += " focus";
+            this.classList.add("focus");
         });
         object.addEventListener('mouseout', function(){
-            this.className = this.className.replace(" focus","");
+            this.classList.remove("focus");
         });
         object.addEventListener('mousedown',function(){
-            this.className += " click";
+            this.classList.add("click");
         });
         object.addEventListener('mouseup', function(){
-            this.className = this.className.replace(" click","");
+            this.classList.remove("click");
+        });
+    }*/
+
+    var accordianElements = document.getElementsByClassName('accordion');
+
+    for(let element of accordianElements){
+        element.addEventListener('mouseover',function(){
+            this.classList.add('active');
+            var panel = this.nextElementSibling;
+            panel.style.maxHeight = panel.scrollHeight + "px";
+ 
+        });
+        element.addEventListener('mouseout',function(){
+            this.classList.remove('active');
+            var panel = this.nextElementSibling;
+            panel.style.maxHeight = null;
         });
     }
 }
 
 //event listener to fire the startup() funciton on load
 window.addEventListener('DOMContentLoaded', startup, false);
+
+//find the elements of the accordian and toggle css class on mouseover
