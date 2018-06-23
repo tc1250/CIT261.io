4ad6c696545921a71a7522c160488316ef3697a7
Tyler Christensen <tcchristensen07@gmail.com>
2018-06-22 18:13:54 -0600

    css animations pages

--- 13. Designing, Defining, and Triggering CSS3 Animations without Custom Libraries (Thought Library)/js.js
+++ 13. Designing, Defining, and Triggering CSS3 Animations without Custom Libraries (Thought Library)/js.js
@@ -1,22 +1,50 @@
 //creates listeners 
 function startup() {
     var target = document.getElementsByTagName('figure');
-   
-    for (let object of target){
-        object.addEventListener('mouseover',function(){
+
+    for (let object of target) {
+        object.addEventListener('mouseover', function () {
             this.className += " focus";
         });
-        object.addEventListener('mouseout', function(){
-            this.className = this.className.replace(" focus","");
+        object.addEventListener('mouseout', function () {
+            this.className = this.className.replace(" focus", "");
         });
-        object.addEventListener('mousedown',function(){
+        object.addEventListener('mousedown', function () {
             this.className += " click";
         });
-        object.addEventListener('mouseup', function(){
-            this.className = this.className.replace(" click","");
+        object.addEventListener('mouseup', function () {
+            this.className = this.className.replace(" click", "");
         });
     }
+
+    var button = document.getElementsByClassName('js-menu-button');
+    for (let object of button) {
+        children = object.childNodes;
+        for (let child of children)
+                child.addEventListener('click', function () {
+                child.classList.toggle('is-active');
+            });
+    }
+
 }
 
 //event listener to fire the startup() funciton on load
 window.addEventListener('DOMContentLoaded', startup, false);
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    var buttons = document.getElementsByClassName('actual-button');
+    for(let element of buttons){
+        element.setAttribute("onClick","closeNav()");
+    }
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    var buttons = document.getElementsByClassName('actual-button');
+    for(let element of buttons){
+        element.setAttribute("onClick","openNav()");
+    }
+}
