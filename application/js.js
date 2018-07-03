d58afaacaaa2dcdcd5d41e20e539271eeb4f0188
Tyler Christensen <tcchristensen07@gmail.com>
2018-07-03 08:35:19 -0600

    testing xmlhttp

--- application/js.js
+++ application/js.js
@@ -1,48 +1,22 @@
 //creates listeners 
 function startup() {
-    var target = document.getElementsByTagName('figure');
-
-    for (let object of target) {
-        object.addEventListener('mouseover', function () {
-            this.className += " focus";
-        });
-        object.addEventListener('mouseout', function () {
-            this.className = this.className.replace(" focus", "");
-        });
-        object.addEventListener('mousedown', function () {
-            this.className += " click";
-        });
-        object.addEventListener('mouseup', function () {
-            this.className = this.className.replace(" click", "");
-        });
-    }
-
-    var video = document.getElementById("myVideo");
-    var btn = document.getElementById("myBtn");
-
-    btn.addEventListener('click', function () {
-        if (video.paused) {
-            video.play();
-            btn.innerHTML = "Pause";
-        } else {
-            video.pause();
-            btn.innerHTML = "Play";
+    //setting up the XMLHTTPRequest
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var responseObj = JSON.parse(this.responseText);
+            document.getElementById('response').innerHTML = JSON.stringify(responseObj, null, 2);
         }
-    });
-    canvas();
+    };
+    
+    xmlhttp.open("GET", 'https://cors-anywhere.herokuapp.com/http://api.walmartlabs.com/v1/search?apiKey=vkyhkt2h8gum2y6bx92yreea&query=ipod', true);
+    //xmlhttp.setRequestHeader('Access-Control-Allow-Origin','*');
+    //xmlhttp.setRequestHeader('X-Originating-IP', '73.65.199.32');
+    //xmlhttp.withCredentials = 'true';
+    xmlhttp.send();
+
 }
 
 //event listener to fire the startup() funciton on load
 window.addEventListener('DOMContentLoaded', startup, false);
 
-//canvas stuff
-function canvas() {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
-    ctx.fillRect(0, 12.5, 800, 75);
-    ctx.font = "16px Arial";
-    ctx.fillStyle='#f1f1f1';
-    ctx.textAlign = "center";
-    ctx.fillText("Created by Tyler Christensen 2018", 400, 56);
-}
