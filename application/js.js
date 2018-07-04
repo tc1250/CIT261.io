8700cbdd82db8f33e04182bd64cf8417ccd0c476
Tyler Christensen <Tyler.Christensen@DEANFLUOR.COM>
2018-07-04 10:21:19 -0600

    testing css setup

--- application/js.js
+++ application/js.js
@@ -1,22 +1,27 @@
-//creates listeners 
-function startup() {
-    //setting up the XMLHTTPRequest
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var responseObj = JSON.parse(this.responseText);
-            document.getElementById('response').innerHTML = JSON.stringify(responseObj, null, 2);
-        }
-    };
-    
-    xmlhttp.open("GET", 'https://cors-anywhere.herokuapp.com/http://api.walmartlabs.com/v1/search?apiKey=vkyhkt2h8gum2y6bx92yreea&query=ipod', true);
-    //xmlhttp.setRequestHeader('Access-Control-Allow-Origin','*');
-    //xmlhttp.setRequestHeader('X-Originating-IP', '73.65.199.32');
-    //xmlhttp.withCredentials = 'true';
-    xmlhttp.send();
-
-}
+function startup(){
+    Sortable.create(results, {
+        handle: '.my-handle',
+        animation: 150
+      });
+};
 
 //event listener to fire the startup() funciton on load
 window.addEventListener('DOMContentLoaded', startup, false);
 
+//AJAX
+function xhr() {
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var responseObj = JSON.parse(this.responseText);
+            var html;
+            for (let object of responseObj.items) {
+                html += '<li><p><span class="my-handle">☰</span>Item ID: ' + object.itemId + '</p><p> Item Name: ' + object.name + '</p><img src="' + object.thumbnailImage + '" height="100px" width="auto"></li>';
+            }
+            document.getElementById('results').innerHTML = html;
+        }
+    };
+    xmlhttp.open("GET", 'https://cors-anywhere.herokuapp.com/http://api.walmartlabs.com/v1/search?apiKey=vkyhkt2h8gum2y6bx92yreea&query=ipod&numItems=3', true);
+    xmlhttp.send();
+
+}
